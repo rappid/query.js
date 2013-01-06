9ec4fa5bf49dd8af3fa50186e0688df191dbdcb6
Tony Findeisen <github@it-ony.de>
2013-01-06 18:20:49 +0100

    added RestConditionParser with tests

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var lib = require("./lib/query.js");
 module.exports = lib.Query;
 module.exports.query = lib.query;
 module.exports.Query = lib.Query;
+
+module.exports.Parser = {
+    RestCondition: require('./lib/RestConditionParser.js').parser
+};
